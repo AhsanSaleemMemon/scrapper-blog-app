25cd6ad90742840d97fcd45d4c0ae5b77164226a
Ahsan Saleem <ahsansaleemmemon@gmail.com>
2021-10-10 10:38:09 +0500

    initial commit

---
+++ app/src/main/java/com/scrapper/i170303_i170364_project/MainActivity.java
@@ -0,0 +1,14 @@
+package com.scrapper.i170303_i170364_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
